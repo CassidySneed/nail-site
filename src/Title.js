8466ae93a1f2ba4becbe32aea9b05ebe4083c012
CassidySneed <teamsneedman@gmail.com>
2021-11-30 16:09:31 -0800

    project init

---
+++ src/Title.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Title.css';
+
+function Title() {
+    return (
+         <div className="Title">
+            <header>
+                <h1>Cassidy's Nailpolish</h1>
+                <small className="Title-Subtitle">Brought to you by Cassidy Sneed</small>
+           </header>
+         </div>
+    )
+  }
+  
+  export default Title
